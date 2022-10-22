e55ad4073e31fc1412eb678a3090872f5c4c8e17
Mick Cox <mcox@deloitte.nl>
2022-10-22 16:19:07 +0200

    Adds exercise 10

---
+++ exercises/solutions/10-http-hello-world.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "hello\n")
+}
+
+func main() {
+
+	http.HandleFunc("/mick", handler)
+
+	fmt.Println("Listening on port 8080...")
+	http.ListenAndServe(":8080", nil)
+}
